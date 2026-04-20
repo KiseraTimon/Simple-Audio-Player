efe8baef57645cba6c5115a4b98f9d395d80b321
KiseraTimon <timonmarick@gmail.com>
2026-04-20 20:43:32 +0300

    Volume Slider

--- script.js
+++ script.js
@@ -256,3 +256,12 @@ muteBtn.addEventListener('click', () => {
         muteBtn.innerHTML = '<polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path>';
     }
 });
+
+volSlider.addEventListener('input', (e) => {
+    audioPlayer.volume = e.target.value;
+    if (audioPlayer.volume > 0) {
+        muteBtn.innerHTML = '<polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path>';
+    } else {
+        muteBtn.innerHTML = '<polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><line x1="23" y1="9" x2="17" y2="15"></line><line x1="17" y1="9" x2="23" y2="15"></line>';
+    }
+});
